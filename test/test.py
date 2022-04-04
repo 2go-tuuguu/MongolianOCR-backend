c84540f111fb59642f281d7d735be508657bd389
Tuguldur Dorjsuren <2go.tuguu@gmail.com>
2022-04-04 16:54:38 +0300

    Upd requirements.txt

--- test/test.py
+++
@@ -1,7 +0,0 @@
-import requests 
-
-# https://your-heroku-app-name.herokuapp.com/predict
-# http://localhost:5000/predict
-resp = requests.post("https://pytorch-flask-2go.herokuapp.com/predict", files={'file': open('eight.png', 'rb')})
-
-print(resp.text)
